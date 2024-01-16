970c527ed26140340ee2616d31d5a577bb4945de
g3vind <g3vind@gmail.com>
2024-01-16 23:14:00 +0530

    design: channel search

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,7 +1,8 @@
-// ResultsPage.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ReactPlayer from "react-player";
+import { RadioTower } from "lucide-react";
+import { FaUserCircle } from "react-icons/fa";
+import { formatRelativeTime } from "../utils/constants";
 
 function ResultsPage() {
   const [videos, setVideos] = useState([]);
@@ -32,20 +33,37 @@ function ResultsPage() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div>
       {videos?.map((video) => (
-        <div key={video.id.videoId}>
-          <ReactPlayer
-            key={video.id.videoId}
-            url={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-            width="560"
-            height="415"
-            style={{ marginTop: "20px", marginLeft: "27vw" }}
-          />
-          {/* <h1>{video?.snippet?.title}</h1>
-          <h1>{video?.snippet?.thumbnails?.medium}</h1>
-          <h1>{video?.snippet?.channelTitle}</h1>
-          <h1>{video?.snippet?.publishTime}</h1> */}
+        <div
+          key={video.id.videoId}
+          className="flex flex-col mt-5 justify-center"
+        >
+          <div className="flex ml-28">
+            <img
+              src={video?.snippet?.thumbnails?.medium?.url}
+              alt="Video Thumbnail"
+              className="inline w-96 rounded-2xl"
+            />
+            <div className="flex flex-col items-center justify-normal mx-3 text-wrap">
+              <div className="flex flex-col gap-2">
+                <h1 className="font-semibold text-lg">
+                  {video?.snippet?.title}
+                </h1>
+                <h1 className="mt-3 flex items-center gap-4">
+                  <FaUserCircle size={27} />
+                  {video?.snippet?.channelTitle}
+                </h1>
+                <h1>{formatRelativeTime(video?.snippet?.publishTime)}</h1>
+                {video?.snippet?.liveBroadcastContent === "live" && (
+                  <button className="bg-red-500 w-16 mt-6 flex items-center px-3 py-2 rounded-xl text-white text-xs">
+                    <RadioTower size={15} />
+                    Live
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
